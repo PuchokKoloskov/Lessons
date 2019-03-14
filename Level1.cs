5c2a4dec12fc2af0211ef0a36f84378be26e6364
Puchok Koloskov <puchok.koloskov@outlook.com>
2019-03-15 00:19:57 +1000

    Level1 commit

---
+++ Level1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Level1Space
+{
+    public static class Level1
+    {
+        public static int squirrel(int N)
+        {
+            decimal factorial = 1;
+
+            for (int i = 1; i <= N; i++)
+            {
+                factorial *= i;
+            }
+
+            decimal firstNumber = factorial;
+
+            while (firstNumber > 10)
+            {
+                firstNumber /= 10;
+            }
+
+            return (int)firstNumber;
+        }
+    }
+}
