035fb1c4a295967cb5f11014f9e901a541ac1e5e
Puchok Koloskov <puchok.koloskov@outlook.com>
2019-03-16 21:31:53 +1000

    Level1 step2 fixed 1

--- Level1.cs
+++ Level1.cs
@@ -13,20 +13,13 @@ namespace Level1Space
             }
             else
             {
-                int tempSpeed = 0;
-                int tempTime = 0;
                 int distance = 0;
 
                 for (int i = 0; i < oksana.Length; i++)
                 {
                     if (i % 2 == 0)
                     {
-                        tempSpeed = oksana[i];
-                    }
-                    else
-                    {
-                        tempTime = oksana[i];
-                        distance += tempSpeed * tempTime;
+                        distance += oksana[i];
                     }
                 }
                 return distance;
