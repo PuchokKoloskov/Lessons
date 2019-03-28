10f9847d5da3b27b4fec567756eee670b28c2db8
Puchok Koloskov <puchok.koloskov@outlook.com>
2019-03-29 00:38:37 +1000

    Level1 step5

--- Level1.cs
+++ Level1.cs
@@ -5,88 +5,66 @@ namespace Level1Space
 {
     public static class Level1
     {
-        public static int[] SynchronizingTables(int N, int[] ids, int[] salary)
+        public static string PatternUnlock(int N, int[] hits)
         {
-            int[] sortedIds = new int[ids.Length];
-            sortedIds = Level1.Sorter(ids);
+            double length = 0;
+            int count = 0;
+            int[] coordinates = new int[hits.Length * 2];
+            int[,] keyBoard = new int[3, 3]
+           {
+                { 6, 1, 9 },
+                { 5, 2, 8 },
+                { 4, 3, 7 }
+           };
 
-            int[] sortedSalaries = new int[ids.Length];
-            sortedSalaries = Level1.Sorter(salary);
+            coordinates = Level1.SearchKey(hits, keyBoard);
 
-            int[] indexesForId = new int[ids.Length];
-            indexesForId = Level1.SearchIndexByEntry(ids, sortedIds);
-
-            int[] reSalary = new int[salary.Length];
-
-            for (int i = 0; i < salary.Length; i++)
+            for (int i = 0; i < N - 1; i++)
             {
-                for (int j = 0; j < salary.Length; j++)
+                if (Math.Abs(coordinates[count] - coordinates[count + 2]) + Math.Abs(coordinates[count + 1] - coordinates[count + 3]) == 1)
                 {
-                    if (indexesForId[i] == j)
-                    {
-                        reSalary[i] = sortedSalaries[j];
-                    }
+                    length += 1;
                 }
+                else if (Math.Abs(coordinates[count] - coordinates[count + 2]) + Math.Abs(coordinates[count + 1] - coordinates[count + 3]) == 2)
+                {
+                    length += Math.Sqrt(2);
+                }
+                else
+                {
+                    return null;
+                }
+                count += 2;
             }
 
-            return reSalary;
+            length = (int)(length * Math.Pow(10, 5));
+            string str = Convert.ToString(length);
+            string hack = str.Trim('0');
+
+            return hack;
         }
 
-        public static int[] Sorter(int[] array)
+        public static int[] SearchKey(int[] hit, int[,] keyBoard)
         {
-            int[] sortArray = new int[array.Length];
-            for (int i = 0; i < array.Length; i++)
-            {
-                sortArray[i] = array[i];
-            }
+            int[] coordinatesOfKeys = new int[hit.Length * 2];
+            int count = 0;
 
-            for (int i = sortArray.Length; i > 0; i--)
+            for (int i = 0; i < hit.Length; i++)
             {
-                for (int j = 0; j < i - 1; j++)
+                for (int j = 0; j < keyBoard.GetLength(0); j++)
                 {
-                    int tempInteger = 0;
-
-                    if (sortArray[j] > sortArray[j + 1])
+                    for (int k = 0; k < keyBoard.GetLength(1); k++)
                     {
-                        tempInteger = sortArray[j];
-                        sortArray[j] = sortArray[j + 1];
-                        sortArray[j + 1] = tempInteger;
-                    }
-                }
-            }
-            return sortArray;
-        }
-
-        public static int[] SearchIndexByEntry(int[] idsOrigin, int[] orderedIds)
-        {
-            int[] indexes = new int[orderedIds.Length];
-
-            for (int i = 0; i < orderedIds.Length; i++)
-            {
-                for (int j = 0; j < idsOrigin.Length; j++)
-                {
-                    if (idsOrigin[j] == orderedIds[i])
-                    {
-                        indexes[i] = j;
-                        break;
+                        if (hit[i] == keyBoard[j, k])
+                        {
+                            coordinatesOfKeys[count] = j;
+                            coordinatesOfKeys[count + 1] = k;
+                            count += 2;
+                        }
                     }
                 }
             }
 
-            int[] idIndexesOrigin = new int[idsOrigin.Length];
-
-            for (int i = 0; i < idsOrigin.Length; i++)
-            {
-                for (int j = 0; j < idsOrigin.Length; j++)
-                {
-                    if (indexes[j] == i)
-                    {
-                        idIndexesOrigin[i] = j;
-                    }
-                }
-            }
-
-            return idIndexesOrigin;
+            return coordinatesOfKeys;
         }
     }
 }
