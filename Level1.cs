e1646ed2775c0531eaafcac92e9f1b0b6e8ed220
Puchok Koloskov <puchok.koloskov@outlook.com>
2019-04-08 22:23:20 +1000

    Level1 step6

--- Level1.cs
+++ Level1.cs
@@ -5,122 +5,21 @@ namespace Level1Space
 {
     public static class Level1
     {
-        public static int[] WordSearch(int len, string s, string subs)
+        public static int SumOfThe(int N, int[] data)
         {
-            if (s.Contains("  "))
+            if (N < 2)
             {
-                return null;
+                return data[0];
             }
 
-            string[] stringArray = Level1._stringSeparator(len, s);
-            char[] charArryay = subs.ToCharArray();
-            int[] intArray = new int[stringArray.Length];
+            int sum = 0;
 
-            for (int i = 0; i < stringArray.Length; i++)
+            for (int i = 0; i < N; i++)
             {
-                char[] tempCharArray = stringArray[i].ToCharArray();
-
-                if (stringArray[i].Contains(subs))
-                {
-                    string[] tempStringArray = stringArray[i].Split(' ');
-
-                    for (int j = 0; j < tempStringArray.Length; j++)
-                    {
-                        if (tempStringArray[j] == subs)
-                        {
-                            intArray[i] = 1;
-                        }
-                    }
-                }
+                sum += data[i];
             }
 
-            return intArray;
-        }
-
-        static private string[] _stringSeparator(int len, string s)
-        {
-            char[] ca = s.ToCharArray();
-            List<char> charList = new List<char>();
-            List<string> stringList = new List<string>();
-
-            for (int i = 0; i < ca.Length; i++)
-            {
-                charList.Add(ca[i]);
-            }
-
-            for (int i = 0; i < s.Length; i++)
-            {
-                int mark = 0;
-                bool flag = false;
-
-                if (charList.Count > len)
-                {
-                    if (charList[len] != ' ')
-                    {
-                        for (int j = len - 1; j > 0; j--)
-                        {
-                            if (charList[j] == ' ')
-                            {
-                                mark = j;
-                                flag = true;
-                                break;
-                            }
-                            else
-                            {
-                                flag = false;
-                            }
-                        }
-
-                        if (flag == false)
-                        {
-                            mark = len;
-                        }
-                    }
-                    else
-                    {
-                        mark = len;
-                    }
-                }
-                else
-                {
-                    mark = charList.Count;
-                }
-
-                char[] tempCa = new char[mark];
-
-                for (int j = 0; j < mark; j++)
-                {
-                    tempCa[j] = charList[j];
-                }
-                stringList.Add(new string(tempCa));
-
-                if (charList.Count > len && charList[mark] == ' ')
-                {
-                    charList.RemoveRange(0, mark + 1);
-                }
-                else if (charList.Count > len)
-                {
-                    charList.RemoveRange(0, mark);
-                }
-                else
-                {
-                    charList.RemoveRange(0, mark);
-                }
-
-                if (charList.Count == 0)
-                {
-                    break;
-                }
-            }
-
-            string[] array = new string[stringList.Count];
-
-            for (int i = 0; i < array.Length; i++)
-            {
-                array[i] = stringList[i];
-            }
-
-            return array;
+            return sum / 2;
         }
     }
 }
