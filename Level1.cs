78397fc6ba074a7acb00ee112624b24e94d0bcf0
Puchok Koloskov <puchok.koloskov@outlook.com>
2019-06-12 22:18:13 +1000

    Level1 step 13 ver1

--- Level1.cs
+++ Level1.cs
@@ -5,50 +5,74 @@ namespace Level1Space
 {
     public static class Level1
     {
-        static public int[] UFO(int N, int[] data, bool octal)
+        static public int Unmanned(int L, int N, int[][] track)
         {
-            int[] resultArray = new int[N];
+            int timeCounter = 0;
+            int roadCounter = 0;
 
-            if (octal)
+            while (roadCounter < L)
             {
-                resultArray = Converter(8, data);
+                for (int j = 0; j < N; j++)
+                {
+                    if (roadCounter == track[j][0])
+                    {
+                        while (!IsGreen(timeCounter, track[j]))
+                        {
+                            timeCounter++;
+                        }
+                    }
+                }
+                timeCounter++;
+                roadCounter++;
+            }
+            return timeCounter;
+        }
+
+        static public bool IsGreen(int timeCounter, int[] track)
+        {
+            bool light = false;
+            int lightCounter = 0;
+
+            if (timeCounter < track[1])
+            {
+                return false;
             }
             else
             {
-                resultArray = Converter(16, data);
-            }
-
-            return resultArray;
-        }
-
-        static public int[] Converter(int notation, int[] data)
-        {
-            int[] result = new int[data.Length];
-
-            for (int i = 0; i < data.Length; i++)
-            {
-                string tempString = data[i].ToString();
-
-                int[] array = new int[tempString.Length];
-
-                for (int j = 0; j < tempString.Length; j++)
+                while (timeCounter > lightCounter)
                 {
-                    array[j] = Convert.ToInt32(tempString.Substring(j, 1));
+                    if (light)
+                    {
+                        for (int i = 0; i < track[1]; i++)
+                        {
+                            if (timeCounter > lightCounter)
+                            {
+                                lightCounter++;
+                            }
+                            else
+                            {
+                                return light;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        for (int i = 0; i < track[2]; i++)
+                        {
+                            if (timeCounter > lightCounter)
+                            {
+                                lightCounter++;
+                            }
+                            else
+                            {
+                                return light;
+                            }
+                        }
+                    }
+                    light = !light;
                 }
-
-                int counter = 0;
-                int sum = 0;
-
-                for (int k = array.Length - 1; k >= 0; k--)
-                {
-                    sum += array[counter] * (int)Math.Pow(notation, k);
-                    counter++;
-                }
-
-                result[i] = sum;
             }
-
-            return result;
+            return light;
         }
     }
 }
