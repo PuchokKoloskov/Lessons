38995fe97cef065dd3de2cbe4251e249149e9aab
Puchok Koloskov <puchok.koloskov@outlook.com>
2019-08-24 13:44:41 +1000

    Level1 step18 ver1

--- Level1.cs
+++ Level1.cs
@@ -5,162 +5,43 @@ namespace Level1Space
 {
     public static class Level1
     {
-        public static List<string> resultList = new List<string>();
-        public static string result = "";
-        public static string indexChar = "";
-        public static bool flag = true;
-        //public static bool intByIndex = false;
-        public static bool undo = false;
-        public static int commandNumber = 0;
-        public static int cursor = 0;
-
-        static public string BastShoe(string command)
+        public static string BiggerGreater(string input)
         {
-            flag = true;
-            CommandControl(command);
-            if (flag)
-            {
-                if (commandNumber == 3)
-                {
-                    return indexChar;
-                }
-                return result;
-            }
-            else
-            {
-                //intByIndex = false;
-                return "";
-            }
-        }
+            char[] ca = input.ToCharArray();
 
-        static public void CommandControl(string command)
-        {
-            if (command.Length <= 1 || command[1] != ' ')
+            for (int i = input.Length - 1; i >= 0; i--)
             {
-                if (command != "4" && command != "5")
+                for (int j = i - 1; j >= 0; j--)
                 {
-                    Console.Write("Неправильные значения!");
-                    flag = false;
-                    return;
+                    if (string.Compare(ca[i].ToString(), ca[j].ToString()) == 1)
+                    {
+                        char temp = ca[i];
+                        ca[i] = ca[j];
+                        ca[j] = temp;
+
+                        sortCharsOnRight(ca, j + 1);
+                        string result = new string(ca);
+                        return result;
+                    }
                 }
             }
-            commandNumber = Convert.ToInt32(command.Substring(0, 1));
-            string content = "";
-            if (commandNumber == 1 || commandNumber == 2 || commandNumber == 3)
-            {
-                content = command.Remove(0, 2);
-            }
-            switch (commandNumber)
-            {
-                case 1:
-                    AddString(content);
-                    break;
-                case 2:
-                    RemoveString(content);
-                    break;
-                case 3:
-                    GetChar(content);
-                    break;
-                case 4:
-                    Undo();
-                    break;
-                case 5:
-                    Redo();
-                    break;
-                default:
-                    Console.WriteLine("Неправильные значения!");
-                    break;
-            }
+            return "";
         }
 
-        public static void AddString(string s)
+        public static void sortCharsOnRight(char[] charArray, int indexOfLeftMutable)
         {
-            if (undo == true)
-            {
-                resultList.Clear();
-                resultList.Add(result);
-                cursor = 1;
-                undo = false;
-            }
-            result += s;
-            resultList.Add(result);
-            cursor++;
-        }
+            char[] tempChar = new char[charArray.Length - indexOfLeftMutable];
 
-        public static void RemoveString(string content)
-        {
-            if (undo == true)
+            for (int i = 0; i < tempChar.Length; i++)
             {
-                resultList.Clear();
-                resultList.Add(result);
-                cursor = 1;
-                undo = false;
+                tempChar[i] = charArray[indexOfLeftMutable + i];
             }
-            int length = 0;
+            Array.Sort(tempChar);
 
-            try
+            for (int i = 0; i < tempChar.Length; i++)
             {
-                length = Convert.ToInt32(content);
+                charArray[i + indexOfLeftMutable] = tempChar[i];
             }
-            catch
-            {
-                Console.WriteLine("Неправильные значения!");
-                flag = false;
-                return;
-            }
-            if (length > result.Length)
-            {
-                result = "";
-                return;
-            }
-            result = result.Remove(result.Length - length);
-            resultList.Add(result);
-            cursor++;
-        }
-
-        public static void GetChar(string content)
-        {
-            //intByIndex = true;
-            int index = 0;
-
-            try
-            {
-                index = Convert.ToInt32(content);
-            }
-            catch
-            {
-                Console.WriteLine("Неправильные значения!");
-                flag = false;
-            }
-            if (index > result.Length - 1)
-            {
-                indexChar = "";
-                return;
-            }
-            char[] caArray = result.ToCharArray();
-            //Console.WriteLine(caArray[index].ToString());
-            indexChar = caArray[index].ToString();
-        }
-
-        public static void Undo()
-        {
-            undo = true;
-            if (cursor <= 1)
-            {
-                return;
-            }
-            result = resultList[cursor - 2];
-            cursor--;
-        }
-
-        public static void Redo()
-        {
-            if (cursor - 1 >= resultList.Count - 1)
-            {
-                return;
-            }
-            result = resultList[cursor];
-            cursor++;
         }
     }
 }
