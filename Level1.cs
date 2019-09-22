ed000585ae637e72c882b3402b95e1c39725300f
Puchok Koloskov <puchok.koloskov@outlook.com>
2019-09-22 16:50:24 +1000

    Level1 step19 ver1

--- Level1.cs
+++ Level1.cs
@@ -5,42 +5,67 @@ namespace Level1Space
 {
     public static class Level1
     {
-        public static string BiggerGreater(string input)
+        public static bool SherlockValidString(string s)
         {
-            char[] ca = input.ToCharArray();
+            bool isValid = false;
 
-            for (int i = input.Length - 1; i >= 0; i--)
-            {
-                for (int j = i - 1; j >= 0; j--)
-                {
-                    if (string.Compare(ca[i].ToString(), ca[j].ToString()) == 1)
-                    {
-                        char temp = ca[i];
-                        ca[i] = ca[j];
-                        ca[j] = temp;
+            List<int> quantitiesListOfChars = new List<int>();
 
-                        sortCharsOnRight(ca, j + 1);
-                        string result = new string(ca);
-                        return result;
-                    }
-                }
-            }
-            return "";
+            char[] caArray = s.ToCharArray();
+            FillTheList(quantitiesListOfChars, caArray);
+            quantitiesListOfChars.Sort();
+
+            isValid = CheckTheValid(quantitiesListOfChars);
+
+            return isValid;
         }
 
-        public static void sortCharsOnRight(char[] charArray, int indexOfLeftMutable)
+        public static void FillTheList(List<int> quantitiesListOfChars, char[] caArray)
         {
-            char[] tempChar = new char[charArray.Length - indexOfLeftMutable];
+            List<char> charList = new List<char>();
 
-            for (int i = 0; i < tempChar.Length; i++)
+            for (int i = 0; i < caArray.Length; i++)
             {
-                tempChar[i] = charArray[indexOfLeftMutable + i];
+                if (charList.Contains(caArray[i]))
+                {
+                    quantitiesListOfChars[charList.IndexOf(caArray[i])]++;
+                }
+                else
+                {
+                    charList.Add(caArray[i]);
+                    quantitiesListOfChars.Add(1);
+                }
             }
-            Array.Sort(tempChar);
+        }
 
-            for (int i = 0; i < tempChar.Length; i++)
+        public static bool CheckTheValid(List<int> quantitiesListOfChars)
+        {
+            if (quantitiesListOfChars[quantitiesListOfChars.Count - 1] - quantitiesListOfChars[0] > 1)
             {
-                charArray[i + indexOfLeftMutable] = tempChar[i];
+                return false;
+            }
+            else
+            {
+                int[] intArray = quantitiesListOfChars.ToArray();
+                char[] caArray = new char[intArray.Length];
+                List<int> quantitiesOfQuantitiesListOfChars = new List<int>();
+
+                for (int i = 0; i < intArray.Length; i++)
+                {
+                    caArray[i] = Convert.ToChar(intArray[i]);
+                }
+
+                FillTheList(quantitiesOfQuantitiesListOfChars, caArray);
+                quantitiesOfQuantitiesListOfChars.Sort();
+
+                if (quantitiesOfQuantitiesListOfChars[0] == 1 || quantitiesOfQuantitiesListOfChars[0] == 1 || quantitiesOfQuantitiesListOfChars.Count == 1)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
             }
         }
     }
