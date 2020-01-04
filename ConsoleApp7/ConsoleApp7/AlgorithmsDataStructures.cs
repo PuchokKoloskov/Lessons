95bb4e9292f2f17c9e1139446c17df5e5f761441
Puchok Koloskov <puchok.koloskov@outlook.com>
2020-01-04 17:49:57 +1000

    Algo step1 ver6

--- ConsoleApp7/ConsoleApp7/AlgorithmsDataStructures.cs
+++ ConsoleApp7/ConsoleApp7/AlgorithmsDataStructures.cs
@@ -111,7 +111,7 @@ namespace AlgorithmsDataStructures
             }
 
             // здесь будет ваш код удаления одного узла по заданному значению
-            return false; // если узел был удалён
+            return false;
         }
 
         public void RemoveAll(int _value)
@@ -205,6 +205,10 @@ namespace AlgorithmsDataStructures
                         {
                             _nodeToInsert.next = head.next;
                             head.next = _nodeToInsert;
+                            if(tail == _nodeAfter)
+                            {
+                                tail = _nodeToInsert;
+                            }
                             return;
                         }
                         else
@@ -225,15 +229,252 @@ namespace AlgorithmsDataStructures
                     }
                     node = node.next;
                 }
-                
                 _nodeToInsert.next = head;
                 head = _nodeToInsert;
-
             }
             // здесь будет ваш код вставки узла после заданного
 
             // если _nodeAfter = null , 
             // добавьте новый элемент первым в списке 
+        }
+
+        public void TestOfRemove()
+        {
+            LinkedList list = new LinkedList();
+            list.Clear();
+
+            Console.WriteLine("Тестирование удаления:");
+
+            if(list.Remove(1) == false)
+            {
+                Console.WriteLine("Удаление из пустого списка корректно!");
+            }
+            else
+            {
+                Console.WriteLine("Удаление из пустого списка некорректно!");
+            }
+
+            list.AddInTail(new Node(10));
+
+            if(list.Remove(10) == true)
+            {
+                if(list.head == null && list.tail == null)
+                {
+                    Console.WriteLine("Удаление единственного узла корректно!");
+                }
+                else
+                {
+                    Console.WriteLine("Удаление единственного узла некорректно!");
+                }
+            }
+
+            list.Clear();
+
+            list.AddInTail(new Node(10));
+            list.AddInTail(new Node(11));
+            list.AddInTail(new Node(12));
+            list.AddInTail(new Node(13));
+            list.AddInTail(new Node(14));
+            list.AddInTail(new Node(15));
+            list.AddInTail(new Node(16));
+            
+
+            if (list.Remove(10) == true)
+            {
+                if(list.head.value == 11)
+                {
+                    Console.WriteLine("Удаление первого узла корректно!");
+                }
+                else
+                {
+                    Console.WriteLine("Удаление первого узла некорректно!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Удаление первого узла некорректно!");
+            }
+
+            if (list.Remove(16) == true)
+            {
+                if (list.tail.value == 15)
+                {
+                    Console.WriteLine("Удаление последнего узла корректно!");
+                }
+                else
+                {
+                    Console.WriteLine("Удаление последнего узла некорректно!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Удаление последнего узла некорректно!");
+            }
+
+        }
+
+        public void TestOfRemoveAll()
+        {
+            LinkedList list = new LinkedList();
+            list.Clear();
+
+            Console.WriteLine("Тестирование удаления всех узлов:");
+
+            list.RemoveAll(10);
+
+            if (list.head == null && list.tail == null)
+            {
+                Console.WriteLine("Удаление всех узлов из пустого списка корректно!");
+            }
+            else
+            {
+                Console.WriteLine("Удаление всех узлов из пустого списка некорректно!");
+            }
+
+            list.AddInTail(new Node(10));
+            list.AddInTail(new Node(11));
+            list.AddInTail(new Node(10));
+            list.AddInTail(new Node(12));
+            list.AddInTail(new Node(10));
+            list.AddInTail(new Node(13));
+
+            list.RemoveAll(10);
+
+            if (list.head.value == 11 && list.head.next.value == 12 && list.tail.value == 13)
+            {
+                Console.WriteLine("Удаление всех узлов корректно!");
+            }
+            else
+            {
+                Console.WriteLine("Удаление всех узлов некорректно!");
+            }
+        }
+
+        public void TestOfFind()
+        {
+            LinkedList list = new LinkedList();
+            list.Clear();
+
+            Console.WriteLine("Тестирование поиска узла:");
+
+            if (list.Find(0) == null)
+            {
+                Console.WriteLine("Поиск в пустом списке корректный");
+            }
+            else
+            {
+                Console.WriteLine("Поиск в пустом списке некорректный");
+            }
+
+            Node testNode = new Node(12);
+
+            list.AddInTail(new Node(10));
+            list.AddInTail(new Node(11));
+            list.AddInTail(testNode);
+
+            if (list.Find(13) == null && list.Find(12) == testNode)
+            {
+                Console.WriteLine("Поиск в непустом списке корректный");
+            }
+            else
+            {
+                Console.WriteLine("Поиск в непустом списке некорректный");
+            }
+        }
+
+        public void TestOfInsertAFter()
+        {
+            LinkedList list = new LinkedList();
+            list.Clear();
+
+            Node testNodeToInsert = new Node(11);
+            Node testNodeAfter = new Node(10);
+
+            Console.WriteLine("Тестирование вставки узла после:");
+
+            list.InsertAfter(testNodeAfter, testNodeToInsert);
+
+            if (list.head == testNodeToInsert && list.tail == testNodeToInsert)
+            {
+                Console.WriteLine("Вставка узла в пустой список корректна");
+            }
+            else
+            {
+                Console.WriteLine("Вставка узла в пустой список некорректна");
+            }
+
+            list.Clear();
+            testNodeAfter.next = null;
+            testNodeToInsert.next = null;
+
+            list.AddInTail(testNodeAfter);
+            list.InsertAfter(testNodeAfter, testNodeToInsert);
+                
+            if (list.head == testNodeAfter && list.tail == testNodeToInsert)
+            {
+                Console.WriteLine("Вставка узла в список с одним узлом корректна");
+            }
+            else
+            {
+                Console.WriteLine("Вставка узла в список с одним узлом некорректна");
+            }
+
+            list.Clear();
+            testNodeAfter.next = null;
+            testNodeToInsert.next = null;
+
+            list.AddInTail(new Node(12));
+            list.AddInTail(new Node(13));
+            list.AddInTail(new Node(14));
+
+            list.InsertAfter(testNodeAfter, testNodeToInsert);
+
+            if (list.head == testNodeToInsert && list.head.next.value == 12 && list.tail.value == 14)
+            {
+                Console.WriteLine("Вставка узла в список с отсутствующим указанным узлом корректна");
+            }
+            else
+            {
+                Console.WriteLine("Вставка узла в список с отсутствующим указанным узлом некорректна");
+            }
+
+            list.Clear();
+            testNodeAfter.next = null;
+            testNodeToInsert.next = null;
+
+            list.AddInTail(testNodeAfter);
+            list.AddInTail(new Node(12));
+            list.AddInTail(new Node(13));
+            list.AddInTail(new Node(14));
+            list.InsertAfter(testNodeAfter, testNodeToInsert);
+
+            if (list.head == testNodeAfter && list.head.next == testNodeToInsert && list.tail.value == 14)
+            {
+                Console.WriteLine("Вставка узла после указанного первого узла корректна");
+            }
+            else
+            {
+                Console.WriteLine("Вставка узла после указанного первого узла некорректна");
+            }
+
+            list.Clear();
+            testNodeAfter.next = null;
+            testNodeToInsert.next = null;
+
+            list.AddInTail(new Node(12));
+            list.AddInTail(new Node(13));
+            list.AddInTail(new Node(14));
+            list.AddInTail(testNodeAfter);
+            list.InsertAfter(testNodeAfter, testNodeToInsert);
+
+            if (list.tail == testNodeToInsert && testNodeAfter.next == testNodeToInsert)
+            {
+                Console.WriteLine("Вставка узла после указанного последнего узла корректна");
+            }
+            else
+            {
+                Console.WriteLine("Вставка узла после указанного последнего узла некорректна");
+            }
         }
     }
 }
