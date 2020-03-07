e389740d70511c8720e370a143e410ef31a0a0db
Puchok Koloskov <puchok.koloskov@outlook.com>
2020-03-07 12:57:44 +1000

    Test for DynArray

--- ConsoleApp7/UnitTestProject1/DynArrayTests.cs
+++ ConsoleApp7/UnitTestProject1/DynArrayTests.cs
@@ -11,26 +11,23 @@ namespace UnitTestProject1
         public void InsertWithoutOverwflow()
         {
             DynArray<int> array = new DynArray<int>();
-
             array.Append(1);
             array.Append(2);
             array.Append(3);
-            array.Append(4);
-            array.Append(5);
-
-            array.Insert(100, 3);
-
-            Assert.IsTrue(array.array[3] == 100 && array.capacity == 16);
+            Assert.IsTrue(array.array[0] == 1 && array.array[1] == 2 && array.array[2] == 3);
+            array.Insert(100, 2);
+            Assert.IsTrue(array.array[2] == 100 && array.capacity == 16);
         }
 
         [TestMethod]
         public void InsertWithOverwflow()
         {
             DynArray<int> array = new DynArray<int>();
-            for (int i = 0; i < 16; i++)
+            for (int i = 1; i <= 16; i++)
             {
                 array.Append(i);
             }
+            Assert.IsTrue(array.array[0] == 1 && array.array[15] == 16);
             array.Insert(100, 7);
             Assert.IsTrue(array.array[7] == 100 && array.capacity == 32);
         }
@@ -42,8 +39,7 @@ namespace UnitTestProject1
             array.Append(1);
             array.Append(2);
             array.Append(3);
-            array.Append(4);
-            array.Append(5);
+            Assert.IsTrue(array.array[0] == 1 && array.array[1] == 2 && array.array[2] == 3);
             Assert.ThrowsException<IndexOutOfRangeException>(() =>
             {
                 array.Insert(100, 10);
@@ -57,24 +53,24 @@ namespace UnitTestProject1
             array.Append(1);
             array.Append(2);
             array.Append(3);
-            array.Append(4);
-            array.Append(5);
+            Assert.IsTrue(array.array[0] == 1 && array.array[1] == 2 && array.array[2] == 3);
 
-            array.Remove(3);
-            Assert.IsTrue(array.array[3] == 5 && array.capacity == 16);
+            array.Remove(1);
+            Assert.IsTrue(array.array[1] == 3 && array.capacity == 16);
         }
 
         [TestMethod]
         public void RemoveWithBufferDecrease()
         {
             DynArray<int> array = new DynArray<int>();
-            for (int i = 0; i < 17; i++)
+            for (int i = 1; i <= 17; i++)
             {
                 array.Append(i);
             }
+            Assert.IsTrue(array.array[0] == 1 && array.array[15] == 16);
             array.Remove(3);
             array.Remove(3);
-            Assert.IsTrue(array.array[3] == 5 && array.capacity == 21);
+            Assert.IsTrue(array.array[3] == 6 && array.capacity == 21);
         }
 
         [TestMethod]
@@ -84,8 +80,7 @@ namespace UnitTestProject1
             array.Append(1);
             array.Append(2);
             array.Append(3);
-            array.Append(4);
-            array.Append(5);
+            Assert.IsTrue(array.array[0] == 1 && array.array[1] == 2 && array.array[2] == 3);
             Assert.ThrowsException<IndexOutOfRangeException>(() =>
             {
                 array.Remove(10);
